e69a4f0e0157bcc692a243b05f339c7dfc077790
Tony Dang <tony@tonydang.blog>
2024-02-21 12:04:29 -0800

    port pwa utils to expo

---
+++ expo-demo/lib/pwa-utils.ts
@@ -0,0 +1,40 @@
+export {
+	//generateEntropy,
+	getDeferred,
+	logError,
+	sanitize,
+	cancelEvt,
+};
+
+// *************************
+
+// TODO: get sodium to work in react-native
+// function generateEntropy(numBytes = 16) {
+// 	return sodium.randombytes_buf(numBytes);
+// }
+
+function getDeferred() {
+	var def = {};
+	def.pr = new Promise((res) => (def.resolve = res));
+	return def;
+}
+
+function logError(err, returnLog = false) {
+	var err = `${err.stack ? err.stack : err.toString()}${
+		err.cause ? `\n${logError(err.cause, /*returnLog=*/ true)}` : ""
+	}`;
+	if (returnLog) return err;
+	else console.error(err);
+}
+
+function sanitize(text) {
+	return text.replace(/[^a-zA-Z0-9\s\.,\/_\-#@!$%^&*~`'"+=:?]+/g, "");
+}
+
+function cancelEvt(evt) {
+	if (evt && evt.preventDefault) {
+		evt.preventDefault();
+		evt.stopPropagation();
+		evt.stopImmediatePropagation();
+	}
+}
