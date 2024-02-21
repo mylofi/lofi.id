3d80852e28a4df54def12a75297eca19050b4c69
Tony Dang <tony@tonydang.blog>
2024-02-21 10:18:07 -0800

    swap out ServerAuth with LocalAuth

--- expo-demo/app/index.tsx
+++ expo-demo/app/index.tsx
@@ -6,40 +6,40 @@ import { ServerAuthSessionContext } from "@/context/ServerAuthSessionContext";
 
 import Account from "@/components/Account";
 import Header from "@/components/Header";
-import ServerAuth from "@/components/ServerAuth";
+import LocalAuth from "@/components/LocalAuth";
 
 export default function Home() {
-  const session = useContext(ServerAuthSessionContext);
+	const session = useContext(ServerAuthSessionContext);
 
-  return (
-    <Box flex={1} backgroundColor="$black">
-      <ScrollView
-        style={{ height: "100%" }}
-        contentContainerStyle={{ flexGrow: 1 }}
-      >
-        <Box
-          position="absolute"
-          $base-h={500}
-          $base-w={500}
-          $lg-h={500}
-          $lg-w={500}
-        >
-          <Gradient />
-        </Box>
+	return (
+		<Box flex={1} backgroundColor="$black">
+			<ScrollView
+				style={{ height: "100%" }}
+				contentContainerStyle={{ flexGrow: 1 }}
+			>
+				<Box
+					position="absolute"
+					$base-h={500}
+					$base-w={500}
+					$lg-h={500}
+					$lg-w={500}
+				>
+					<Gradient />
+				</Box>
 
-        <Box
-          height="60%"
-          $base-my="$16"
-          $base-mx="$5"
-          $base-height="80%"
-          $lg-my="$24"
-          $lg-mx="$5"
-          justifyContent="space-between"
-        >
-          <Header />
-          {session && session.user ? <Account /> : <ServerAuth />}
-        </Box>
-      </ScrollView>
-    </Box>
-  );
+				<Box
+					height="60%"
+					$base-my="$16"
+					$base-mx="$5"
+					$base-height="80%"
+					$lg-my="$24"
+					$lg-mx="$5"
+					justifyContent="space-between"
+				>
+					<Header />
+					{session && session.user ? <Account /> : <LocalAuth />}
+				</Box>
+			</ScrollView>
+		</Box>
+	);
 }
