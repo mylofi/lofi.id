b50aa39ab762ae5153f75077d6c9ddd8535338c2
Tony Dang <tony@tonydang.blog>
2024-02-25 10:28:36 -0800

    add welcome screen

---
+++ expo-demo/app/welcome/index.tsx
@@ -0,0 +1,42 @@
+import { Stack, router } from "expo-router";
+import { Button, ButtonText, VStack } from "@gluestack-ui/themed";
+
+import Header from "@/components/Header";
+import Layout from "@/components/Layout";
+
+export default function Welcome() {
+	return (
+		<>
+			<Stack.Screen options={{ headerTitle: "Home" }} />
+			<Layout>
+				<VStack space="4xl">
+					<Header>Local-First Auth</Header>
+
+					<VStack space="sm" m="$3">
+						<Button
+						// onPress={() => router.navigate("/welcome/login")}
+						>
+							<ButtonText>Login</ButtonText>
+						</Button>
+
+						<Button
+							onPress={() => router.navigate("/welcome/register")}
+						>
+							<ButtonText>Register</ButtonText>
+						</Button>
+
+						<Button
+						// onPress={() => router.navigate("/welcome/receive-sync")}
+						>
+							<ButtonText>Receive Sync</ButtonText>
+						</Button>
+
+						<Button onPress={() => router.navigate("/welcome/pwa")}>
+							<ButtonText>PWA</ButtonText>
+						</Button>
+					</VStack>
+				</VStack>
+			</Layout>
+		</>
+	);
+}
