46d4ab477b7010fda7581e1485ee9991543a0e85
Tony Dang <tony@tonydang.blog>
2024-02-25 10:13:08 -0800

    add stack info

--- expo-demo/app/(app)/index.tsx
+++ expo-demo/app/(app)/index.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { router } from "expo-router";
+import { Stack, router } from "expo-router";
 import { Button, ButtonText, Heading, VStack } from "@gluestack-ui/themed";
 
 import { LoginSessionContext } from "@/context/LoginSessionContext";
@@ -9,25 +9,28 @@ export default function AppIndex() {
 	const { setLoginSession } = useContext(LoginSessionContext);
 
 	return (
-		<Layout>
-			<VStack space="4xl">
-				<Heading color="$white" size="2xl">
-					You are logged in
-				</Heading>
+		<>
+			<Stack.Screen options={{ headerTitle: "Account" }} />
+			<Layout>
+				<VStack space="4xl">
+					<Heading color="$white" size="2xl">
+						You are logged in
+					</Heading>
 
-				<Button
-					size="md"
-					variant="solid"
-					action="primary"
-					onPress={() => {
-						// TODO: Clear from device storage. See clearLoginSession.
-						setLoginSession(null);
-						router.navigate("/welcome");
-					}}
-				>
-					<ButtonText>Log Out</ButtonText>
-				</Button>
-			</VStack>
-		</Layout>
+					<Button
+						size="md"
+						variant="solid"
+						action="primary"
+						onPress={() => {
+							// TODO: Clear from device storage. See clearLoginSession.
+							setLoginSession(null);
+							router.navigate("/welcome");
+						}}
+					>
+						<ButtonText>Log Out</ButtonText>
+					</Button>
+				</VStack>
+			</Layout>
+		</>
 	);
 }
