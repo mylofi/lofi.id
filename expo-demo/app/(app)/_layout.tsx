99089cf04e6b2d4c8afcc590cca79884cee24ae8
Tony Dang <tony@tonydang.blog>
2024-02-25 08:09:27 -0800

    redirect to /welcome if not logged in

--- expo-demo/app/(app)/_layout.tsx
+++ expo-demo/app/(app)/_layout.tsx
@@ -16,7 +16,7 @@ export default function AppLayout() {
 	if (!session) {
 		// On web, static rendering will stop here as the user is not authenticated
 		// in the headless Node process that the pages are rendered in.
-		return <Redirect href="/sign-in/" />;
+		return <Redirect href="/welcome/" />;
 	}
 
 	// This layout can be deferred because it's not the root layout.
