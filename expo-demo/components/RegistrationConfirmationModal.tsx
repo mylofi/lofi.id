3fefcbaedf144b20becdaa86781789e84beba527
Tony Dang <tony@tonydang.blog>
2024-02-25 09:50:22 -0800

    add RegistrationConfirmationModal

--- expo-demo/components/RegistrationConfirmationModal.tsx
+++ expo-demo/components/RegistrationConfirmationModal.tsx
@@ -27,4 +27,47 @@ export default function RegistrationConfirmationModal({
 	loginKeyWords: string;
 	modalVisible: boolean;
 }) {
+	const { loginSession } = useContext(LoginSessionContext);
+
+	return (
+		<AlertDialog isOpen={modalVisible}>
+			<AlertDialogBackdrop />
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<VStack space="xs">
+						<HStack space="sm" alignItems="center">
+							{/* <Icon
+									as={CheckCircleIcon}
+									color="$success700"
+									$dark-color="$success300"
+								/> */}
+							<Heading size="lg">Profile Registered</Heading>
+						</HStack>
+
+						<Text size="sm">
+							These words are your unique login ID for profile '
+							{loginSession?.profileName}'. You'll need them each
+							time you login!
+						</Text>
+					</VStack>
+				</AlertDialogHeader>
+				<AlertDialogBody>
+					<Textarea size="sm" isReadOnly={true}>
+						<TextareaInput value={loginKeyWords} />
+					</Textarea>
+				</AlertDialogBody>
+				<AlertDialogFooter borderTopWidth="$0">
+					{/* TODO: Add copy to clipboard button? */}
+					<Button
+						variant="solid"
+						size="sm"
+						action="primary"
+						onPress={() => router.navigate("/")}
+					>
+						<ButtonText>Login</ButtonText>
+					</Button>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
 }
