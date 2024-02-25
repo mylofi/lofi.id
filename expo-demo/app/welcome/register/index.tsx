56e3a53e74f06f8fcb1e5895996a14eeaf13702f
Tony Dang <tony@tonydang.blog>
2024-02-25 10:38:56 -0800

    add imports

---
+++ expo-demo/app/welcome/register/index.tsx
@@ -0,0 +1,23 @@
+import { useContext, useState } from "react";
+import { Stack, router } from "expo-router";
+import {
+	Button,
+	ButtonText,
+	FormControl,
+	FormControlError,
+	FormControlErrorText,
+	FormControlLabel,
+	FormControlLabelText,
+	Heading,
+	Input,
+	InputField,
+	VStack,
+} from "@gluestack-ui/themed";
+
+import { readStoredProfiles } from "@/lib/pwa-app";
+import { sanitize } from "@/lib/pwa-utils";
+
+import { ProfileNameContext } from "@/context/ProfileNameContext";
+
+import Layout from "@/components/Layout";
+
