9a8e8763a1d7cd6513f79dfb361996454446918c
Tony Dang <tony@tonydang.blog>
2024-02-26 14:43:23 -0800

    update LoginSession type

--- expo-demo/context/LoginSessionContext.ts
+++ expo-demo/context/LoginSessionContext.ts
@@ -2,6 +2,18 @@ import { createContext } from "react";
 
 import type { Dispatch, SetStateAction } from "react";
 
+export interface keyInfo {
+	iv: Uint8Array;
+	publicKey: Uint8Array;
+	privateKey: Uint8Array;
+	encPK: Uint8Array;
+	encSK: Uint8Array;
+}
+
+export interface UnpackedLoginSession extends keyInfo {
+	profileName: string;
+}
+
 export interface LoginSession {
 	profileName: string;
 	iv: string;
