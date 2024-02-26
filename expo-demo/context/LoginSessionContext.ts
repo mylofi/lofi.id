5187bc3b58fa894ba82404dc64e5916a0836f588
Tony Dang <tony@tonydang.blog>
2024-02-26 14:43:39 -0800

    update LoginSession type

--- expo-demo/context/LoginSessionContext.ts
+++ expo-demo/context/LoginSessionContext.ts
@@ -23,17 +23,6 @@ export interface LoginSession {
 	encSK: string;
 }
 
-// TODO: Use this type instead of the LoginSession in all the places the session
-// key info is not packed yet.
-export interface UnpackedLoginSession {
-	profileName: string;
-	iv: Uint8Array;
-	publicKey: Uint8Array;
-	privateKey: Uint8Array;
-	encPK: Uint8Array;
-	encSK: Uint8Array;
-}
-
 export const LoginSessionContext = createContext<{
 	loginSession: LoginSession | null;
 	setLoginSession: Dispatch<SetStateAction<LoginSession | null>>;
